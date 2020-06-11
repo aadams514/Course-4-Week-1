8b0df4c5ee843127c2b9739f416369ff36fed69a
aadams514 <58942642+aadams514@users.noreply.github.com>
2020-06-10 20:12:53 -0400

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -9,11 +9,13 @@ unzip(zipfile = "exdata_data_household_power_consumption", exdir = "ExplorQ1Data
 
 ##Read Table
 Q1<-read.table("household_power_consumption.txt", skip = 1, sep = ";")
+
 ##set names of columns
 names(Q1)<-c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 
 ##make Date and Time data point
 subQ1<-unite(subQ1, "Date_Time", Date:Time, remove = FALSE)
+
 ##display date and time in year, month, day, and hour, min, sec format
 subQ1$Date_Time<-ymd_hms(subQ1$Date_Time)
 
